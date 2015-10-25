bdf01c2c2d25ebf899fa7c2c199a6933e69bc048
BlackGanglion <BlackGanglion@blackganglion.com>
2015-10-25 17:43:14 +0800

    revise
    revise

--- 101-clickable-grid/淡苍-clickable-grid/main.js
+++ 101-clickable-grid/淡苍-clickable-grid/main.js
@@ -6,8 +6,9 @@ $('#n-submit').on('click', function() {
   var n = parseInt($('#n-size').val());
 
   //添加格子
+  var table = " ";
   for(var i = 1; i <= n; i++){
-    var line = " ";
+    line = '<ul>';
     for(var j = 1; j <= n; j++){
       var number = (i - 1) * n + j;
       line = line
@@ -15,17 +16,19 @@ $('#n-submit').on('click', function() {
           + number
           + '</li>';
     }
-    $('.main').append(
-      '<ul>' + line + '</ul>'
-    );
+    line = line + '</ul>';
+    table = table + line;
   }
+
+  //统一dom渲染
+  $('.main').append(table);
 
   //根据n值调整字体大小
   var font_size = Math.ceil(150 / n) + 'px';
   $('li').css('font-size', font_size);
 
   //为格子注册事件
-  $(".main").delegate("li", "click", function(){
+  $(".main").on("click", "li", function(){
     var elementId = $(this).attr('id');
     console.log(elementId);
   });
