216e5828b56b2575aac4196bafc56c53847ef50e
newraina <newraina@qq.com>
2015-10-25 22:24:05 +0800

    添加功能：当点击每个格子的时候，都用console.log打印出当前格子的数字 其他修复

--- 101-clickable-grid/main.js
+++ 101-clickable-grid/main.js
@@ -2,63 +2,44 @@
  * Created by newraina on 2015/10/24 0024.
  */
 
-box = document.getElementsByClassName('box')[0];
-select = document.getElementById('select');
+var box = document.getElementsByClassName('box')[0];
+var select = document.getElementById('select');
+var item = document.getElementsByClassName('item');
+
 
 // 事件绑定
-select.addEventListener('change', function(){update(select.value)}, false);
+window.addEventListener('load', function(){update(2)});
+select.addEventListener('change', function(){update(this.value)});
+
+
+
 
 function update(value) {
 
     // 清空所有小方块
     box.innerHTML = '';
 
+    // 生成格子
     for (var i = 0; i < value; i++) {
         var newColumn = document.createElement('div');
         newColumn.setAttribute('class', 'column');
         box.appendChild(newColumn);
         for (var j = 0; j < value; j++) {
-            var new_item = document.createElement('span');
-            new_item.setAttribute('class', 'item');
-            changeFontSize(new_item, value);
-            var numnode = document.createTextNode((i * value + j + 1).toString());
-            new_item.appendChild(numnode);
-            newColumn.appendChild(new_item);
+            var newItem = document.createElement('span');
+            newItem.setAttribute('class', 'item');
+            changeFontSize(newItem, value);
+            var numNode = document.createTextNode((i * value + j + 1).toString());
+            newItem.appendChild(numNode);
+            newColumn.appendChild(newItem);
+            //  点击每个格子的时候，用console.log打印出当前格子的数字
+            newItem.addEventListener('click', function(){console.log(this.innerHTML)});
         }
     }
+
+
 }
 
 function changeFontSize(node, value) {
     // 根据方块个数调整字体大小
-    switch (value) {
-        case '1':
-            node.style.fontSize = '200px';
-            break;
-        case '2':
-            node.style.fontSize = '100px';
-            break;
-        case '3':
-            node.style.fontSize = '80px';
-            break;
-        case '4':
-            node.style.fontSize = '50px';
-            break;
-        case '5':
-            node.style.fontSize = '35px';
-            break;
-        case '6':
-            node.style.fontSize = '30px';
-            break;
-        case '7':
-            node.style.fontSize = '25px';
-            break;
-        case '8':
-            node.style.fontSize = '20px';
-            break;
-        case '9':
-            node.style.fontSize = '17px';
-            break;
-        default :
-            break;
-    }
+    node.style.fontSize = (200 / value).toString() + 'px';
 }
