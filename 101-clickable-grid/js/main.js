9ab84d694161247be12f38beb84ed2a0e0162d22
laoxubuer <cheerxl@hotmail.com>
2015-10-27 11:46:17 +0800

    add a comma

--- 101-clickable-grid/js/main.js
+++ 101-clickable-grid/js/main.js
@@ -1,13 +1,13 @@
 /**
  * Created by xuliang on 15/10/25.
  */
-(function ($) {
+(function($) {
     var $geneBtn = $("#generate-btn"),
         $gridMain = $("#grid-main");
-    $geneBtn.on('click', function () {
-        $gridMain.empty();//清除原有内容
-        var i, j
-        len = $(":text").val();//获取基数
+    $geneBtn.on('click', function() {
+        $gridMain.empty(); //清除原有内容
+        var i, j,
+            len = $(":text").val(); //获取基数
         /*生成六行六列*/
         for (i = 0; i < len; i++) {
             var $gridItem = "";
@@ -23,8 +23,7 @@
         $gridMain.css("fontSize", fontSize);
     });
     /*为每一个方格添加事件代理*/
-    $gridMain.on('click', 'li', function () {
+    $gridMain.on('click', 'li', function() {
         console.log($(this).text());
     });
 })(jQuery);
-
