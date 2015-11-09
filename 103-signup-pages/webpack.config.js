380827faeeab7cafb5d6b9354c85cb0541789e51
newraina <newraina@qq.com>
2015-11-09 14:23:53 +0800

    update
    提交必要文件

---
+++ 103-signup-pages/webpack.config.js
@@ -0,0 +1,38 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: {
+//    'main': './js/main.js',
+    'style': './css/main.scss'
+  },
+
+  output: {
+  	path: 'out',
+    filename: '[name].js'
+  },
+
+module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
+      },
+      //.js 文件使用 jsx-loader 来编译处理
+      { 
+      	test: /\.js$/, 
+      	loader: 'jsx-loader?harmony' 
+      },
+      //图片文件使用 url-loader 来处理，小于8kb的直接转为base64
+      { 
+      	test: /\.(png|jpg|woff|woff2|eot|ttf|svg)$/, 
+      	loader: 'url-loader?limit=8192'
+      }
+    ]
+  },
+
+  plugins: [
+    new ExtractTextPlugin('[name].css', {
+      allChunks: true
+    })
+  ]
+};
