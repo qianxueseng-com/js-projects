66efa20d7152edaa465ae695f6c7c67a6889e194
heee <136645138@qq.com>
2016-01-24 15:34:20 +0800

    重写

--- 103-signup-pages/script.js
+++ 103-signup-pages/script.js
@@ -2,61 +2,22 @@
  * @Author: hefei
  * @Date:   2015-12-21 10:09:36
  * @Last Modified by:   hefei
- * @Last Modified time: 2015-12-26 22:22:19
+ * @Last Modified time: 2016-01-24 15:22:10
  */
-
 'use strict';
 jQuery(document).ready(function($) {
-	var phoneNumPattern = /\d{11}/;
-	$('#phoneNumber').change(function(event) {
-		var phoneNum = $('#phoneNumber').val().toString();
+	var $mobile = $('#mobile'),
+		$mobileWrong = $('#mobileWrong');
+	var phoneNumPattern = /^\d{11}$/; //简单的手机号码正则
+	//当输入框内容改变时出发检测
+	$mobile.change(function(event) {
+		var phoneNum = $mobile.val().toString();
 		if (phoneNum != '') {
 			if (!phoneNumPattern.test(phoneNum)) {
-				$('#phoneWrong').css("display", "inline");
+				$mobileWrong.show();
 			} else {
-				$('#phoneWrong').css("display", "none");
+				$mobileWrong.hide();
 			}
 		}
-	});
-	var moveBoxLeft = $('#showbox').offset().left;
-	$('#showbox').mousedown(function(event) {
-		var moveBox = $('#showbox');
-		var initX = moveBox.offset().left;
-		var mouseX = event.pageX;
-		moveBox.mousemove(function(event) {
-			var len = parseInt(event.pageX) - parseInt(mouseX)
-			if (event.pageX >= moveBoxLeft + 18 && event.pageX <= moveBoxLeft + 245) {
-				var temX = len + parseInt(initX);
-				moveBox.css("left", temX + "px");
-				$('#showleft').css({
-					"width": len + "px",
-					"height": "35px"
-			});
-			}
-			if(event.pageX >= moveBoxLeft + 240){
-				showImg();
-			}
-		}).mouseup(function(event) {
-			moveBox.unbind('mousemove');
-		}).mouseleave(function(event) {
-			moveBox.unbind('mousemove');
-		});
-	});
-	function showImg (argument) {
-		var img = '<img src="img/showImg.png"/>'
-		$('#showImg').show();
-		$('#showbox').empty().append(img);
-		$('#showbox').unbind('mousemove');
-	}
-
-	$('#freshImg').click(function(event) {
-		var random = (Math.ceil(Math.random() * 3)).toString();
-		$('#verifyImg').attr({
-			src: "img/" + random + '.jpg'
-		});
-	})
-
-	$('#nextStep').click(function() {
-
 	});
 });
