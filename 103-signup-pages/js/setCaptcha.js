780d21af8e9bee64315098acf9d0aa173acfc8ce
DuXingXia233 <1071501458@qq.com>
2016-02-24 00:26:33 +0800

    修正验证码BUG
    补充了备用资源  解决了静态页面使用ajax请求资源的失败的问题

--- 103-signup-pages/js/setCaptcha.js
+++ 103-signup-pages/js/setCaptcha.js
@@ -25,7 +25,7 @@ function setCaptcha($container) {
 			success: function(result) {
 				var data = result.data;
 
-				captcha = data[Math.floor(Math.random()*data.length)];
+					captcha = data[Math.floor(Math.random()*data.length)];
 
 
 				// 设置验证图片源
@@ -37,7 +37,93 @@ function setCaptcha($container) {
 
 			},
 			error: function(){
-				$text.text('网络出现问题，请点击"看不清？换一张"');
+				var backUps = [{
+						"data": [{
+							"value": "当",
+							"xl": "102",
+							"yt": "122",
+							"xr": "124",
+							"yb": "145"
+								}, {
+							"value": "缩",
+							"xl": "160",
+							"yt": "231",
+							"xr": "182",
+							"yb": "253"	
+								}, {
+							"value": "快",
+							"xl": "261",
+							"yt": "234",
+							"xr": "283",
+							"yb": "255"	
+								}, {
+							"value": "收",
+							"xl": "35",
+							"yt": "245",
+							"xr": "57",
+							"yb": "266"	
+								}]
+						},{
+						"data": [{
+							"value": "内",
+							"xl": "168",
+							"yt": "48",
+							"xr": "190",
+							"yb": "69"
+								},{
+							"value": "工",
+							"xl": "156",
+							"yt": "99",
+							"xr": "178",
+							"yb": "119"
+								},{
+							"value": "鲁",
+							"xl": "194",
+							"yt": "223",
+							"xr": "216",
+							"yb": "243"
+								},{
+							"value": "大",
+							"xl": "231",
+							"yt": "82",
+							"xr": "253",
+							"yb": "105"
+								}]
+						},{
+						"data": [{
+							"value": "女",
+							"xl": "151",
+							"yt": "104",
+							"xr": "173",
+							"yb": "128"
+								},{
+							"value": "至",
+							"xl": "200",
+							"yt": "113",
+							"xr": "222",
+							"yb": "137"
+								},{
+							"value": "比",
+							"xl": "174",
+							"yt": "190",
+							"xr": "196",
+							"yb": "214"
+								},{
+							"value": "般",
+							"xl": "221",
+							"yt": "277",
+							"xr": "243",
+							"yb": "299"
+								}]
+						}
+					];
+				var data = backUps[captchaIndex].data;
+
+					captcha = data[Math.floor(Math.random()*data.length)];
+				
+				$target.attr("src", './source/captcha/captcha0'+captchaIndex+".png");
+				$text.text('静态网络(测试码)，请点击图中的“'+captcha.value+'”字');
+
 			},
 
 			datatype: "json"
